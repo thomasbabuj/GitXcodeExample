7b8a7047808568ced356b1cfa6c76dad9ae68533
Thomas Babu <joseph.thomas.babu@gmail.com>
2013-07-14 12:44:57 +0800

    Removed extra space

---
+++ GitUseCaseExample/MapForIternerary.h
@@ -0,0 +1,15 @@
+//
+//  MapForIternerary.h
+//  GitUseCaseExample
+//
+//  Created by Publicis on 14/7/13.
+//  Copyright (c) 2013 Thomas. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MapForIternerary : NSObject
+
+
+
+@end
--- GitUseCaseExample/ViewController.h
+++ GitUseCaseExample/ViewController.h
@@ -10,6 +10,4 @@
 
 @interface ViewController : UIViewController
 - (IBAction)clickTheButton:(id)sender;
-
-
 @end
